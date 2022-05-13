6f00a6e693e52ea15fe1577e9e5dba09d99f5c3c
BishyanKar <bishyankar99@gmail.com>
2022-05-13 11:01:00 +0530

    profile UI changes

--- app/src/main/java/com/example/smartbin/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/smartbin/ui/profile/ProfileFragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.example.smartbin.Constants
 import com.example.smartbin.R
@@ -37,6 +38,9 @@ class ProfileFragment : Fragment() {
 
     private lateinit var transactionAdapter: TransactionAdapter
 
+    private var totalScans = 0
+    private var totalWeights = 0.0
+
     private val profileViewModel by viewModels<ProfileViewModel>()
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -57,25 +61,57 @@ class ProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         editor = sharedPreferences.edit()
+        toggleProgressBar(true)
         initRecyclerView()
         initUser()
+    }
+
+    private fun toggleProgressBar(flag: Boolean) {
+        if(flag) {
+            binding.progressCircular.visibility = View.VISIBLE
+            binding.nestedScrollView.visibility = View.GONE
+        }
+        else {
+            binding.progressCircular.visibility = View.GONE
+            binding.nestedScrollView.visibility = View.VISIBLE
+        }
     }
 
     private fun initRecyclerView() {
         transactionAdapter = TransactionAdapter(requireContext())
         binding.rvHistory.adapter = transactionAdapter
+        binding.rvHistory.layoutManager = LinearLayoutManager(context)
     }
 
     private fun getAllTransactions() {
         profileViewModel.getAllTransactions().observe(viewLifecycleOwner, Observer { response ->
             if(response.body!=null) {
                 val transactionResponse = response.body
-                transactionAdapter.submitList(transactionResponse.transactions)
+                val transactions = transactionResponse.transactions
+                totalScans = transactions?.size!!
+                binding.tvScanCount.text = "$totalScans"
+                if(transactions.isEmpty()){
+                    binding.tvHistoryPlaceholder.text = context?.getString(R.string.no_records)
+                    binding.tvHistoryPlaceholder.visibility = View.VISIBLE
+                }
+                else {
+                    binding.tvHistoryPlaceholder.visibility = View.GONE
+                    for(transaction in transactions!!){
+                        totalWeights += transaction.weight!!
+                    }
+                }
+
+                binding.tvWeightMeter.text = "$totalWeights kg"
+
+                transactionAdapter.submitList(transactions)
             }
             else {
                 Timber.e(response.errorMessage)
+                binding.tvHistoryPlaceholder.text = "Some error occurred"
+                binding.tvHistoryPlaceholder.visibility = View.VISIBLE
                 Toast.makeText(context, "${response.errorMessage}", Toast.LENGTH_SHORT).show()
             }
+            toggleProgressBar(false)
         })
     }
 
